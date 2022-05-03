61b0b886e6352e7959de009d1ee383a5f391673f
Tomasz Kawalec <tkawalec369@gmail.com>
2022-05-03 11:02:06 +0200

    Remove lots of commented out code

--- luisp.lua
+++ luisp.lua
@@ -10,7 +10,6 @@ local function printTab(tab, depth)
 	if depth == nil then depth = 0 end
 
 	if type(tab) == "table" then
-		-- for i = 1, depth do io.write("  ") end
 		print("{")
 		for k, v in pairs(tab) do
 			for i = 1, (depth + 1) do io.write("  ") end
@@ -25,26 +24,6 @@ local function printTab(tab, depth)
 end
 
 --http://lua-users.org/wiki/CopyTable
--- local function deepcopy(orig, copies)
--- 	copies = copies or {}
--- 	local orig_type = type(orig)
--- 	local copy
--- 	if orig_type == 'table' then
--- 		if copies[orig] then
--- 			copy = copies[orig]
--- 		else
--- 			copy = {}
--- 			copies[orig] = copy
--- 			for orig_key, orig_value in next, orig, nil do
--- 				copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
--- 			end
--- 			setmetatable(copy, deepcopy(getmetatable(orig), copies))
--- 		end
--- 	else -- number, string, boolean, etc
--- 		copy = orig
--- 	end
--- 	return copy
--- end
 local function deepcopy(orig)
 	local orig_type = type(orig)
 	local copy
@@ -106,21 +85,6 @@ local luispCoreFunctions = {
 					else
 						sum = sum + v.value
 					end
-					-- elseif v.type == "list" then
-					-- 	local res, err, errDetail = luispModule.exec(v, true)
-					-- 	if err then
-					-- 		return nil, "" .. err, errDetail
-					-- 	else
-					-- 		if res.type == "atom" then
-					-- 			if tonumber(res.value) == nil then
-					-- 				return nil, "TypeError", "All arguments in + function should be number atoms or (callable) lists that return number atom"
-					-- 			else
-					-- 				sum = sum + res.value
-					-- 			end
-					-- 		else
-					-- 			return nil, "TypeError", "All arguments in + function should be number atoms or (callable) lists that return number atom"
-					-- 		end
-					-- 	end
 				else
 					if debugMode then
 						print("Stack: " .. debug.traceback())
@@ -137,8 +101,8 @@ local luispCoreFunctions = {
 	{
 		name = "-",
 		callback = function(args)
-			-- print("Subtracting")
 			if debugMode then
+				print("Subtracting")
 				print("Subtracting args: ")
 				printTab(args)
 			end
@@ -153,34 +117,12 @@ local luispCoreFunctions = {
 				if k == 1 then
 					if v.type == "atom" then
 						sub = v.value
-						-- elseif v.type == "list" then
-						-- 	local res, err, errDetail = luispModule.exec(v, true)
-						-- 	if err then
-						-- 		return nil, "" .. err, errDetail
-						-- 	else
-						-- 		if res.type == "atom" then
-						-- 			sub = res.value
-						-- 		else
-						-- 			return nil, "TypeError", "All arguments in + function should be atoms or (callable) lists that return atom"
-						-- 		end
-						-- 	end
 					else
 						return nil, "TypeError", "All arguments in + function should be atoms or (callable) lists that return atom"
 					end
 				else
 					if v.type == "atom" then
 						sub = sub - v.value
-						-- elseif v.type == "list" then
-						-- 	local res, err, errDetail = luispModule.exec(v, true)
-						-- 	if err then
-						-- 		return nil, err, errDetail
-						-- 	else
-						-- 		if res.type == "atom" then
-						-- 			sub = sub - res.value
-						-- 		else
-						-- 			return nil, "TypeError", "All arguments in + function should be atoms or (callable) lists that return atom"
-						-- 		end
-						-- 	end
 					else
 						return nil, "TypeError", "All arguments in + function should be atoms or (callable) lists that return atom"
 					end
@@ -206,16 +148,6 @@ local luispCoreFunctions = {
 			for k, v in pairs(args.value) do
 				if v.type == "atom" or v.type == "quotelist" then
 					table.insert(result.value, v)
-					-- elseif v.type == "list" then
-					-- 	local res, err, errDetail = luispModule.exec(v, true)
-					-- 	if err then
-					-- 		-- if printErrors then
-					-- 		-- 	print("Error: ", err, errDetail)
-					-- 		-- end
-					-- 		return nil, "" .. err, errDetail
-					-- 	else
-					-- 		table.insert(result.value, res)
-					-- 	end
 				end
 			end
 			return result
@@ -235,17 +167,8 @@ local luispCoreFunctions = {
 			end
 
 			if args.value[1].type == "atom" then
-				-- if args.value[2].type == "list" then
-				-- 	local res, err, errDetail = luispModule.exec(args.value[2], true)
-				-- 	if err then
-				-- 		return nil, "" .. err, errDetail
-				-- 	else
-				-- 		luispVariables[args.value[1].value] = res
-				-- 	end
-				-- else
 				local copy = deepcopy(args.value[2])
 				luispVariables[args.value[1].value] = copy
-				-- end
 			else
 				return nil, "TypeError", "Argument 1 should be atom"
 			end
@@ -372,42 +295,17 @@ local luispIOFunctions = {
 				printTab(args)
 			end
 			if args.value[1].type == "atom" then
-				-- print("Printing atom: " .. args[1].value)
+				if debugMode then
+					print("Printing atom: " .. args[1].value)
+				end
 				print(args.value[1].value)
 			elseif args.value[1].type == "quotelist" then
 				for k, v in ipairs(args.value[1].value) do
 					local res, err, errDetail = luispModule.exec({ type = "list", value = { { type = "atom", value = "print" }, v } }, true)
 					if err then
-						-- if printErrors then
-						-- 	print("Error: ", err, errDetail)
-						-- end
 						return nil, "" .. err, errDetail
 					end
 				end
-				-- elseif args.value[1].type == "list" then
-				-- 	if debugMode then
-				-- 		print("Printing list:")
-				-- 		printTab(args.value[1])
-				-- 	end
-				-- 	local res, err, errDetail = luispModule.exec(args.value[1], true)
-				-- 	if err then
-				-- 		-- if printErrors then
-				-- 		-- 	print("Error: ", err, errDetail)
-				-- 		-- end
-				-- 		return nil, "" .. err, errDetail
-				-- 	else
-				-- 		if debugMode then
-				-- 			print("Res:")
-				-- 			printTab(res)
-				-- 		end
-				-- 		_, err, errDetail = luispModule.exec({ type = "list", value = { { type = "atom", value = "print" }, res } }, true)
-				-- 		if err then
-				-- 			-- if printErrors then
-				-- 			-- 	print("Error: ", err, errDetail)
-				-- 			-- end
-				-- 			return nil, "" .. err, errDetail
-				-- 		end
-				-- 	end
 			end
 			return nil
 		end
@@ -443,7 +341,6 @@ function luispModule.parse(code)
 				parserState.inString = false
 				local currentList = parsedCode
 				for i = 1, parserState.indentLvl do
-					-- currentList = currentList[#currentList]
 					currentList = currentList.value[#currentList.value]
 				end
 				table.insert(currentList.value, { type = "atom", value = parserState.stringBuf })
@@ -462,11 +359,7 @@ function luispModule.parse(code)
 				else
 					local currentList = parsedCode
 					for i = 1, parserState.indentLvl do
-						-- if i == 1 then
-						-- 	currentList = currentList[#currentList]
-						-- else
 						currentList = currentList.value[#currentList.value]
-						-- end
 					end
 					table.insert(currentList.value, { type = listtype, value = {} })
 				end
@@ -478,11 +371,7 @@ function luispModule.parse(code)
 				if parserState.stringBuf ~= "" then
 					local currentList = parsedCode
 					for i = 1, parserState.indentLvl do
-						-- if i == 1 then
-						-- 	currentList = currentList[#currentList]
-						-- else
 						currentList = currentList.value[#currentList.value]
-						-- end
 					end
 					table.insert(currentList.value, { type = "atom", value = parserState.stringBuf })
 					if debugMode then print("Leaving atom: \"" .. parserState.stringBuf .. "\"") end
@@ -499,11 +388,7 @@ function luispModule.parse(code)
 				--Leaving atom
 				local currentList = parsedCode
 				for i = 1, parserState.indentLvl do
-					-- if i == 1 then
-					-- 	currentList = currentList[#currentList]
-					-- else
 					currentList = currentList.value[#currentList.value]
-					-- end
 				end
 				table.insert(currentList.value, { type = "atom", value = parserState.stringBuf })
 				if debugMode then print("Leaving atom: \"" .. parserState.stringBuf .. "\"") end
